28feb248590c3cab6a84523803d1317ed374e378
Artem Aminov <artemaminov@gmail.com>
2014-09-20 21:35:39 +0600

    fix main-image hover js

--- app/assets/javascripts/store/product_variant_options.js
+++ app/assets/javascripts/store/product_variant_options.js
@@ -6,18 +6,16 @@ var show_variant_images = function(variant_id) {
   // hide it and select the first available thumb instead.
   // if(!currentThumb.hasClass('tmb-' + variant_id)) {
     var thumb = $($('ul.thumbnails li:visible').eq(0));
-    var thumb = $($("ul.thumbnails li.vtmb tmb-" + variant_id + ":first").eq(0));
-    thumb = $($('ul.thumbnails li:visible').eq(0));
+//    var thumb = $($("ul.thumbnails li.vtmb tmb-" + variant_id + ":first").eq(0));
+//    thumb = $($('ul.thumbnails li:visible').eq(0));
     var newImg = thumb.find('a').attr('href');
-    if(!newImg)
-    {
-      newImg = '/default.png'
+    if (newImg) {
+        $('ul.thumbnails li').removeClass('selected');
+        thumb.addClass('selected');
+        $('#main-image img').attr('src', newImg);
+        $("#main-image").data('selectedThumb', newImg);
+        $("#main-image").data('selectedThumbId', thumb.attr('id'));
     }
-    $('ul.thumbnails li').removeClass('selected');
-    thumb.addClass('selected');
-    $('#main-image img').attr('src', newImg);
-    $("#main-image").data('selectedThumb', newImg);
-    $("#main-image").data('selectedThumbId', thumb.attr('id'));
   // }
   // currentThumb.find('img').attr('src', $('li.vtmb.tmb-' + variant_id).first().find('a img').parent().data('zoom-image'))
 }
