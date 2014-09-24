27d53dd248d3b0f53323e999f4f4636a7d13039d
Artem Aminov <artemaminov@gmail.com>
2014-09-24 21:58:24 +0600

    add CloudZoom support

--- app/assets/javascripts/store/product_variant_options.js
+++ app/assets/javascripts/store/product_variant_options.js
@@ -1,23 +1,38 @@
-var show_variant_images = function (variant_id) {
-    $('li.vtmb').hide();
-    $('li.vtmb.tmb-' + variant_id).show();
-    var currentThumb = $('#main-image');
+show_variant_images = function(variantId) {
+    hide_all_variant_images();
+
+    var currentVariant = $("li.tmb-" + variantId),
+        currentImageWrapper = $("#main-image"),
+        currentMainImage = currentImageWrapper.find("img"),
+        currentThumb = currentImageWrapper.data("selectedThumb"),
+        currentThumbId = currentImageWrapper.data("selectedThumbId");
+
+    currentVariant.show();
+
     // if currently selected thumb does not belong to current variant, nor to common images,
     // hide it and select the first available thumb instead.
-    // if(!currentThumb.hasClass('tmb-' + variant_id)) {
-    var thumb = $($('ul.thumbnails li:visible').eq(0));
-//    var thumb = $($("ul.thumbnails li.vtmb tmb-" + variant_id + ":first").eq(0));
-//    thumb = $($('ul.thumbnails li:visible').eq(0));
-    var newImg = thumb.find('a').attr('href');
-    if (newImg) {
-        $('ul.thumbnails li').removeClass('selected');
-        thumb.addClass('selected');
-        $('#main-image img').attr('src', newImg);
-        currentThumb.data('selectedThumb', newImg);
-        currentThumb.data('selectedThumbId', thumb.attr('id'));
+    if (!$("#" + currentThumbId).hasClass(".tmb-" + variantId)) {
+        var newThumb = $("#product-images ul.thumbnails li:visible.selected").eq(0);
     }
-    // }
-    // currentThumb.find('img').attr('src', $('li.vtmb.tmb-' + variant_id).first().find('a img').parent().data('zoom-image'))
+    if (!(newThumb.length > 0)) {
+        var newThumb = $("#product-images ul.thumbnails li:visible.vtmb").eq(0);
+    }
+
+    var newThumbLink = newThumb.find("a"),
+        newImage = newThumbLink.attr("href"),
+        dataCloudZoom = newThumbLink.data("cloudzoom");
+
+    if (currentThumb && dataCloudZoom) {
+        $('ul.thumbnails li').removeClass("selected");
+        newThumb.addClass("selected");
+        currentImageWrapper.data('selectedThumb', newImage);
+        currentImageWrapper.data('selectedThumbId', newThumb.attr('id'));
+        currentMainImage.attr('src', newImage);
+        currentMainImage.attr('data-cloudzoom', dataCloudZoom);
+        currentMainImage.data("CloudZoom").loadImage(newImage, newThumbLink.data("zoomimage"));
+//        currentMainImage.data("CloudZoom").destroy();
+//        CloudZoom.quickStart();
+    }
 };
 
 var hide_all_variant_images = function () {
