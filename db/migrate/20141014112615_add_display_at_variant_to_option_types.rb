55118f6805017c0ca1b34730c3e4435a4346169f
Artem Aminov <artemaminov@gmail.com>
2015-10-20 18:42:18 +0500

    fix migration

--- db/migrate/20141014112615_add_display_at_variant_to_option_types.rb
+++ db/migrate/20141014112615_add_display_at_variant_to_option_types.rb
@@ -1,9 +1,9 @@
 class AddDisplayAtVariantToOptionTypes < ActiveRecord::Migration
   def up
-    add_column :display_at_variant, :boolean, :default => false
+    add_column :spree_option_types, :display_at_variant, :boolean, :default => false
   end
 
   def down
-    remove_column :display_at_variant
+    remove_column :spree_option_types, :display_at_variant
   end
 end
